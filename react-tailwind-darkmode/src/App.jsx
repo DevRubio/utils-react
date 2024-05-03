275acdf68a569d5dfd6f616d36e01cf3976b8d3f
Oscar Rubio <ofrubio0@gmail.com>
2024-05-02 20:02:35 -0500

    (UPDATE) Se agrega icon al boton y se crea estado para cambiar el icon al momento de cambiar el theme

--- react-tailwind-darkmode/src/App.jsx
+++ react-tailwind-darkmode/src/App.jsx
@@ -2,20 +2,26 @@ import { useEffect, useState } from "react"
 
 
 const App = () => {
+  const [icon, setIcon] = useState('')
   const [theme, setTheme] = useState(()=>{
     //Se utiliza para tomar el theme del sistema
     if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setIcon('sun')
       return "dark"
     }
+    setIcon('moon')
     return "light"
   })
+  
 
   useEffect(()=>{
     const themeUser = localStorage.getItem('themeUser')
     if(themeUser === 'dark'){
       document.querySelector('html').classList.add('dark')
+      setIcon('sun')
     }else{
       document.querySelector('html').classList.remove('dark')
+      setIcon('moon')
     }
   },[theme])
 
@@ -28,7 +34,9 @@ const App = () => {
       <button 
       className='bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900'
       onClick={handleChangeTheme}
-      >Change Theme</button>
+      >Change Theme 
+      <i className={`fa-solid fa-${icon} fa-xl p-2`}></i>
+      </button>
     </div>
   )
 }
