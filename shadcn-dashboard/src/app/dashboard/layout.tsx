8a0ca630687fbf26814d40c79cff10fc70b0accc
ofrubio0 <ofrubio0@gmail.com>
2024-06-01 21:57:57 +0000

    (ADD) Se agrega el componente contextMenu

--- shadcn-dashboard/src/app/dashboard/layout.tsx
+++ shadcn-dashboard/src/app/dashboard/layout.tsx
@@ -17,7 +17,8 @@ const links = [
   { name : "carousel", href: "carousel"},
   { name : "checkbox", href: "checkbox"},
   { name : "command", href: "command"},
-  { name : "combobox", href: "combobox"}
+  { name : "combobox", href: "combobox"},
+  { name : "contextMenu", href: "contextMenu"},
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
